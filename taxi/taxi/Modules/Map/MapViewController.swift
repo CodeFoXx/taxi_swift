09bf3e89583a28c0745f6010087f78275be02ffa
Polina Osina <osinapm@sib-soft.ru>
2018-03-27 13:57:53 +0700

    change settings on MapVC

--- taxi/taxi/Modules/Map/MapViewController.swift
+++ taxi/taxi/Modules/Map/MapViewController.swift
@@ -22,18 +22,18 @@ class MapViewController: MapsAPIViewController {
         //self.apiParams["mode"] = "debug"
         
         // Default: "ru_RU".
-        self.apiParams["lang"] = "en_US"
+        self.apiParams["lang"] = "ru_RU"
         
         // Default: "release".
         self.apiParams["mode"] = "debug"
         
         // Default: "2.1".
-//        self.apiVersion = "2.1-dev"
-//        
-//        // Default: false. See https://tech.yandex.com/maps/doc/jsapi/2.1/commercial/index-docpage/
-//        self.apiEnterprise = true
-//        self.apiParams["apikey"] = "your_key"
-//        
+        self.apiVersion = "2.1-dev"
+        
+        // Default: false. See https://tech.yandex.com/maps/doc/jsapi/2.1/commercial/index-docpage/
+        //self.apiEnterprise = true
+        //self.apiParams["apikey"] = "your_key"
+        
 //        // By default is taken from view controller name. I.e. "myMap" for MyMapViewController.
 //        self.jsFileName = "map"
         
